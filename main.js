bf60b3c0234482edf79150a1b6b279aca40a44ce
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-18 06:36:53 -0300

    Armazenando lista de teclas em uma const

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
-}
-document.querySelector(".tecla_pom").onclick = tocaSomPom;
-
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
