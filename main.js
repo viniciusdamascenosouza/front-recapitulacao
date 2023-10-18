ae801930d8a785a61c0bea9bb72184e9cb7c4410
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-18 06:58:20 -0300

    Percorrendo listas

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function tocaSomPom() {
+function tocaSom(tecla) {
   document.querySelector("#som_tecla_pom").play();
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  listaDeTeclas[contador].onclick = tocaSom;
+
+  contador = contador + 1;
+
+  console.log(contador);
+}
