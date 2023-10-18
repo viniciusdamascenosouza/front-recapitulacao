a22f8a331383d3df3ff26e9333585776619b4875
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-10-18 14:39:00 -0300

    Limpando código

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ function tocaSom(idElementAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
+
+for (let c = 0; c < listaDeTeclas.length; c++) {
+  const tecla = listaDeTeclas[c];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+}
+
+// UTILIZANDO WHILE
 // let contador = 0;
 
 // while (contador < listaDeTeclas.length) {
@@ -19,15 +33,3 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 
 //   contador = contador + 1;
 // }
-
-for (contador = 0; contador < listaDeTeclas.length; contador++) {
-  const tecla = listaDeTeclas[contador];
-
-  const instrumento = tecla.classList[1];
-
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.onclick = function () {
-    tocaSom(idAudio);
-  };
-}
